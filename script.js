81cca97f622144d67205cebfc1eb55dcab19ba49
Rohan Ambhore <142285944+rohan1207@users.noreply.github.com>
2025-02-10 17:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++){
+		if(str.charAt(i)==str.charAt(i+1)){
+			return null;
+		}
+		else{
+			return str.charAt(i);
+		}
+	}
+	
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
